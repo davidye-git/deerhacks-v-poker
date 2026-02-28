bc2dc18c1cb1e7b111712854d36eb4c05398481a
Youcheng (David) Ye <86391860+davidye-git@users.noreply.github.com>
2026-02-28 17:37:49 -0500

    Add files via upload

--- player.py
+++ player.py
@@ -1,6 +1,9 @@
 import pydealer
 
 class Player:
+    """
+    A regular player in a poker game.
+    """
     player_name: str
     hand: list[pydealer.Card]
 
@@ -14,7 +17,7 @@ class Player:
         self.hand = []
         self.cash = 1000
         self.bet_amount = 0
-        self.status = ""
+        self.status = "playing"
 
     def __str__(self) -> str:
         return (f"Player {self.player_name} has status {self.status} and cash "
@@ -24,19 +27,23 @@ class Player:
         """
         Change <self.status> for the current turn.
 
-        Precondition: <status> is either "check", "call", "raise", or "fold".
+        Precondition: <status> is either "playing", or "folded".
         """
         self.status = status
 
-    def bet(self, amount: int) -> None:
+    def bet(self, amount: int) -> int:
         self.bet_amount = amount
         self.cash -= amount
+        return amount
 
     def new_round(self) -> None:
         self.hand = []
         self.bet_amount = 0
-
+        self.change_status("playing")
 
 class MrCasino(Player):
-    def __init__(self, player_name: str) -> None:
-        super().__init__(player_name)
+    """
+    The overpowered bot poker player.
+    """
+    def __init__(self) -> None:
+        super().__init__("Mr. Casino")
